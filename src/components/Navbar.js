da9bcd754b6bcec1f0fcf9677f9c912f7dff30a1
DebjyotiBhattacharjee <debjyotibhattacharjee010@gmail.com>
2022-06-01 19:55:00 +0530

    third commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,12 +23,12 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-               <Link className="nav-link" aria-current="page" to="/"> 
+               <Link className="nav-link" aria-current="page" to="TextUtilsReact/"> 
                 Home
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/about">
+              <Link className="nav-link" to="TextUtilsReact/about">
                 {props.aboutText}
               </Link>
             </li>
